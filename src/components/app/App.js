7699fc200696edb9a1cc3f2c8dd9f308c2a06b2e
Alexander Ilinykh <AlexanderIlinykhDev@yandex.ru>
2022-01-24 20:04:01 +0500

    folders with lowercase letter, added app.module.css

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,26 +1,10 @@
+import Header from "../header/header";
+
+import appStyles from "./app.module.css";
 function App() {
   return (
     <div className="App">
-      <h1>
-        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Optio in
-        commodi temporibus cum, veniam ullam error sit velit nobis ipsum quis
-        facilis quidem maxime qui magni repudiandae! Veritatis, aspernatur. In.
-      </h1>
-      <h2>
-        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Optio in
-        commodi temporibus cum, veniam ullam error sit velit nobis ipsum quis
-        facilis quidem maxime qui magni repudiandae! Veritatis, aspernatur. In.
-      </h2>
-      <h3>
-        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Optio in
-        commodi temporibus cum, veniam ullam error sit velit nobis ipsum quis
-        facilis quidem maxime qui magni repudiandae! Veritatis, aspernatur. In.
-      </h3>
-      <p>
-        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Optio in
-        commodi temporibus cum, veniam ullam error sit velit nobis ipsum quis
-        facilis quidem maxime qui magni repudiandae! Veritatis, aspernatur. In.
-      </p>
+      <Header />
     </div>
   );
 }
