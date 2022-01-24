8dbd60cbcb9906314630c8c4c8433377fc125577
Alexander Ilinykh <AlexanderIlinykhDev@yandex.ru>
2022-01-24 19:21:32 +0500

    added fonts

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,9 +1,26 @@
-import "./App.css";
-
 function App() {
   return (
     <div className="App">
-      <header className="App-header">React app</header>
+      <h1>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Optio in
+        commodi temporibus cum, veniam ullam error sit velit nobis ipsum quis
+        facilis quidem maxime qui magni repudiandae! Veritatis, aspernatur. In.
+      </h1>
+      <h2>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Optio in
+        commodi temporibus cum, veniam ullam error sit velit nobis ipsum quis
+        facilis quidem maxime qui magni repudiandae! Veritatis, aspernatur. In.
+      </h2>
+      <h3>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Optio in
+        commodi temporibus cum, veniam ullam error sit velit nobis ipsum quis
+        facilis quidem maxime qui magni repudiandae! Veritatis, aspernatur. In.
+      </h3>
+      <p>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Optio in
+        commodi temporibus cum, veniam ullam error sit velit nobis ipsum quis
+        facilis quidem maxime qui magni repudiandae! Veritatis, aspernatur. In.
+      </p>
     </div>
   );
 }
